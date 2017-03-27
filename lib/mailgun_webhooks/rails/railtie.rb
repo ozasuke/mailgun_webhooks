8bdb9870f4c737bf56f8cf0b6b5872f880d03b91
ozasuke <oz.boc.l.3t.0409w@gmail.com>
2017-03-27 11:32:21 +0900

    support rails 5.1

--- lib/mailgun_webhooks/rails/railtie.rb
+++ lib/mailgun_webhooks/rails/railtie.rb
@@ -2,7 +2,7 @@ module MailgunWebhooks
   module Rails
     class Railtie < ::Rails::Railtie
       initializer "mailgun_webhooks.insert_middleware" do |app|
-        app.config.middleware.use "MailgunWebhooks::Rack"
+        app.config.middleware.use MailgunWebhooks::Rack
       end
     end
   end
